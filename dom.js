61f27a97a082683d92d6f558c5300aeaa016087c
ozlemezgi <ozlemezgisari@icloud.com>
2022-09-10 21:07:09 +0300

    dom getElementById get ElementsByTagName

--- dom.js
+++ dom.js
@@ -1,3 +1,15 @@
-document.getElementById("bio").innerHTML="Ezgi Sari : 1998"
+// document.getElementById("bio").innerHTML="Ezgi Sari : 1998"
+//innerHTML p nin İÇİ yani metin yazılan yer.
 
-//innerHTML p nin içi yani metin yazılan yer.
+var intro1 = document.getElementById("intro1");
+
+var mesaj = document.getElementById("mesaj");
+mesaj.innerHTML = intro1.innerHTML;
+
+var allLists = document.getElementsByTagName("ul");  //ul tagine ulaştım
+var cities = allLists[0] // hangi ul olduğunu belirtiyoruz.tüm listelerin 0. sı
+allElements = cities.getElementsByTagName("li");  // ul'nin altındaki li lere ulaştık
+for (i = 0; i < allElements.length; i++) {   
+    alert(allElements[i].innerHTML);    //innerHTML li nin içini okumamızı sağlar.
+}
+
